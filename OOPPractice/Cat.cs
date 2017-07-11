41b3b9943f673743d4dfb9d1f48b7f24105b4287
mjthomas22 <mthomas22@gmail.com>
2017-07-11 10:06:35 -0400

    added fields to class

---
+++ OOPPractice/Cat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Cat
+    {
+        //Inside our class we create a blueprint for our object.
+        //Classes are templates in OOP that we use to create objects
+        //They should always be named in Pascal Case
+
+        //In OOP there are two types of states:
+        //Fields variablies that belong to a class
+
+        private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+        //properties are a mix between field and methods that allow controlled access
+        public Name
+            {
+                get {return this.name;}
+                set {this.name = ValueType
+            }
+    //We also have behaviors
+    //Methods - repeatable/reusable sections of code - Actions
+    //and Constructors - Specialized methods that are used when instantiating an object
+    //          -Allows us to control the state of an object upon creation
+    //          Allow us to perform any actions the object might need upon creation
+    //          Always share the name of the class
+    public Cat()
+    {
+        //This is an example of a default constructor a default takes no arguements/parameters
+    }
+
+
+
+}
+}
--- OOPPractice/Program.cs
+++ OOPPractice/Program.cs
@@ -10,6 +10,20 @@ namespace OOPPractice
     {
         static void Main(string[] args)
         {
+            //When we intialize a new object, it is called instantiating (creating a new instance)
+            //An object needs to be instantiated before it can be used
+            //All objects of a specific type are created from the same template - CLASS
+            // All objects from a class will have the same number and type of states, 
+            //But each instance can have its own unique values
+
+
+            //the format we use to instantiate an object looks like this:
+            //ClassName objectName = new ClassName();
+            Cat mittens = new Cat();
+            //To create a new object, we use a special method called a CONSTUCTOR
+
+            //Thinking about object in real like, create (either hand-write or type)
+            //three examples of objects, they should have five states and five behaviors
         }
     }
 }
